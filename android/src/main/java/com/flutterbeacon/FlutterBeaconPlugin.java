77e6b4da64b0af9e734890a483cffe70c9262d99
yb0x00 <yeongbeen0x00@gmail.com>
2025-08-07 14:03:54 +0900

    refactor: ios 빌드 확인 완료

--- android/src/main/java/com/flutterbeacon/FlutterBeaconPlugin.java
+++ android/src/main/java/com/flutterbeacon/FlutterBeaconPlugin.java
@@ -20,7 +20,7 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.PluginRegistry;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
+//import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 public class FlutterBeaconPlugin implements FlutterPlugin, ActivityAware, MethodCallHandler,
     PluginRegistry.RequestPermissionsResultListener,
@@ -54,12 +54,12 @@ public class FlutterBeaconPlugin implements FlutterPlugin, ActivityAware, Method
 
   }
 
-  public static void registerWith(Registrar registrar) {
-    final FlutterBeaconPlugin instance = new FlutterBeaconPlugin();
-    instance.setupChannels(registrar.messenger(), registrar.activity());
-    registrar.addActivityResultListener(instance);
-    registrar.addRequestPermissionsResultListener(instance);
-  }
+  // public static void registerWith(Registrar registrar) {
+  //   final FlutterBeaconPlugin instance = new FlutterBeaconPlugin();
+  //   instance.setupChannels(registrar.messenger(), registrar.activity());
+  //   registrar.addActivityResultListener(instance);
+  //   registrar.addRequestPermissionsResultListener(instance);
+  // }
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
